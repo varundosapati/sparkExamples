b97f4cd2ebfc39aa3ad1a91255cc3d7e406acbf8
Varun Dosapati <vspsvk@gmail.com>
2019-11-26 16:57:50 -0600

    Working on module8

--- sparkExamples/src/main/java/org/hadoopexam/spark/core/module8.scala
+++ sparkExamples/src/main/java/org/hadoopexam/spark/core/module8.scala
@@ -2,6 +2,18 @@ package org.hadoopexam.spark.core
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+/*
+ * reduce(_+_) : accepts two elements and result as the same type
+ * fold(initialVla)(_+_) : accepts two elements with initial value and result as the same type
+ * Usage of Fold over reduce - When we get an empty collection reduce throw exception instead if we use fold it throws initial value
+ *  
+ * aggregate() : accepts two function input aggregate can have different result type
+ * 
+ * accumalator : As in hadoop world as counter , accumalator gets (writes)incremented in each worker and cannot read each other accumalator, and driver only reads the accumalator and can get final sum 
+ * Usage of accumalator - Used for dbugging purpose, long running jobs , can be most used effective for counter
+ * 
+ * 
+ */
 
 object module8 {
 
@@ -39,7 +51,29 @@ object module8 {
 // same thing for multiplication
 //val y = x.reduce(_ * _) 
 // y: Int = 3628800
-   
+  
+  /*
+   * Example of using fold action on integer 
+   */
+  
+  val foldReq = sc.parallelize(List(1, 2, 3, 4), 2)
+  
+  val foldRes = foldReq.fold(1)(_ + _)
+  
+  println("Result of fold initial value for each partition "+foldRes)
+  
+   /*
+    * Example of using fold on string 
+    */
+  
+  val foldreqStr = sc.parallelize(List("a", "b", "c", "d"), 3)
+  
+  val foldResStr = foldreqStr.fold("z")(_+_)
+  
+  println("Result of fold initial value z and three partitions "+foldResStr)
+  
   }
+  
+  
   
 }
